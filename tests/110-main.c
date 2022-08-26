f96696d73a0fa08845ea4fa6ae2318b3a4fa496c
Oyizaadewuyi <oyizaadewuyi@gmail.com>
2022-08-26 00:43:55 +0000

    Binary Trees

---
+++ tests/110-main.c
@@ -0,0 +1,50 @@
+
+bdbaraban
+/
+0x1C-binary_trees
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+0x1C-binary_trees/tests/110-main.c
+@234761
+234761 Collectively fix file path to header inclusion
+ 1 contributor
+Executable File  33 lines (29 sloc)  880 Bytes
+#include <stdlib.h>
+#include <stdio.h>
+#include "../binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    binary_tree_t *root;
+    int bst;
+
+    root = binary_tree_node(NULL, 98);
+    root->left = binary_tree_node(root, 12);
+    root->right = binary_tree_node(root, 128);
+    root->left->right = binary_tree_node(root->left, 54);
+    root->right->right = binary_tree_node(root, 402);
+    root->left->left = binary_tree_node(root->left, 10);
+
+    binary_tree_print(root);
+    bst = binary_tree_is_bst(root);
+    printf("Is %d bst: %d\n", root->n, bst);
+    bst = binary_tree_is_bst(root->left);
+    printf("Is %d bst: %d\n", root->left->n, bst);
+
+    root->right->left = binary_tree_node(root->right, 97);
+    binary_tree_print(root);
+    bst = binary_tree_is_bst(root);
+    printf("Is %d bst: %d\n", root->n, bst);
+    return (0);
+}
